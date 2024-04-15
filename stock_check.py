837d4759ab2d94b067e1c1ce2a5be17e06f8f2ef
danialgi <163375828+danialgi@users.noreply.github.com>
2024-04-15 13:31:09 +0800

    Update stock_check.py

--- stock_check.py
+++ stock_check.py
@@ -77,7 +77,7 @@ quantity_erp = st.selectbox('ERP QUANTITY column:', df3.columns.tolist())
 
 df1_filtered = df1[product_wms].unique()
 df1_filtered = pd.DataFrame(df1_filtered, columns=['product_wms'])
-df1_filtered = df1_filtered.sort_values(by=product_wms, ascending=True)
+#df1_filtered = df1_filtered.sort_values(by=product_wms, ascending=True)
 df1_filtered.reset_index(inplace=True)
 df1_filtered = df1_filtered.drop('index', axis=1)
 df1_filtered
